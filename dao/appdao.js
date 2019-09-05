734e5b0ee35a8b800a768b6ff10a063cc836762d
KIM <kkw01234@gmail.com>
2019-09-05 22:08:53 +0900

    add: annotation

--- dao/appdao.js
+++ dao/appdao.js
@@ -3,6 +3,14 @@ const sqlite3 = require('sqlite3').verbose();
 
 const appDAO = {
     db : new sqlite3.Database(dbname),
+    /**
+     * returns appropriate results for the query statement
+     * 
+     * @param {String} query sql grammar
+     * @param {Array} parmas array of variables
+     * @returns {Promise} promise with results
+     * 
+     */
     get(query, params = []){
        return new Promise((resolve,reject)=>{
            this.db.get(query,params, (err, result)=>{
@@ -12,6 +20,15 @@ const appDAO = {
        }) ;
         
     },
+    /**
+     * return objects correspond to the query statement
+     * query문의 모든 row를 반환해주는 함수
+     * 
+      @param {String} query sql grammar
+     * @param {Array} parmas array of variables
+     * @returns {Promise} promise with results
+     * 
+     */
     all(query, params = []){
         return new Promise((resolve,reject)=>{
             this.db.all(query, params, function(err, rows){
@@ -21,6 +38,15 @@ const appDAO = {
             });
         });
     },
+    /**
+     * 
+     * execute query statemet (결과값이 없는 insert, update)
+     * 
+      @param {String} query sql grammar
+     * @param {Array} parmas array of variables
+     * @returns {Promise} if query is execute correctly, return true else return error
+     * 
+     */
     run(query, params = []){
         return new Promise((resolve, reject)=>{
             this.db.run(query, params, function(err){
--- repository/userRepository.js
+++ repository/userRepository.js
@@ -1,18 +1,47 @@
 const { query } = require("../db/database.js");
 const sqlite3 = require('sqlite3').verbose();
 class UserRepository{
+    /**
+     * @constructor
+     * @param {dao} dao can execute query statment
+     */
+    
     constructor(dao){
         this.dao = dao;
     }
+    /**
+     * if not exist user table, create user table
+     * @return {result} if query is execute correctly, return true. else return err
+     * 
+     */
     async makeUser() {
         return this.dao.run(query.createUserTable());
     }
+    /**
+     * 
+     * @param {String} id user's id
+     * @returns {String} query result 
+     * 
+     */
     async checkId(id) {
         return this.dao.all(query.checkId(), [id]);
     }
+    /**
+     * insert user in the database
+     * @param {Object} user 
+     * @returns {Boolean} if query is execute correctly, return true else return error
+     * 
+     */
     async insertUser(user){
         return this.dao.run(query.regtsterUser(),[user.id,user.password,user.name,[user.birthYear,user.birthMonth,user.birthDate].join("-"),user.gender,user.email,user.phone,user.interests])
     }
+    /**
+     * if user want to log in, check id and password
+     * 
+     * @param {String} id input user's id
+     * @param {String} password input user's password
+     * @returns {String} if id and password exist in database, return user's object 
+     */
     async checkUser(id, password){
         return this.dao.all(query.checkUser(),[id, password]);
     }
